8c2e59622bb116eb260b44051425ef08ed3d7121
MohamedTouaa <121357213+MohamedTouaa@users.noreply.github.com>
2024-10-13 17:04:35 +0100

    mayne 3

--- Assets/UI/Scripts/LoadScene.cs
+++ Assets/UI/Scripts/LoadScene.cs
@@ -3,7 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    
+    public void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
     public void LoadSceneByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
